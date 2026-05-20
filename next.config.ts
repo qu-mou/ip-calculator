7a69fd96736f0966accf3c90bea71084c6b080a2
qu-mou <142991103+qu-mou@users.noreply.github.com>
2026-05-20 16:25:54 +0800

    Modify Next.js config for standalone output
    Updated Next.js configuration to use standalone output and TypeScript settings.

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,11 @@
 import type { NextConfig } from "next";
 
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',  // 启用静态导出
-  // 如果有图片优化，需要禁用或配置 unoptimized
-  images: {
-    unoptimized: true,
+const nextConfig: NextConfig = {
+  output: "standalone",
+  typescript: {
+    ignoreBuildErrors: true,
   },
-  // 如果你的项目不是部署在域名根目录，需要设置 basePath
-  // basePath: '/your-repo-name',
-}
+  reactStrictMode: false,
+};
 
-module.exports = nextConfig
+export default nextConfig;
